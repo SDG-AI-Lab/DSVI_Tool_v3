efe19bb2c388d00ea1cde18080eed97e2c3410c4
imsakararyal <imsakararyal@gmail.com>
2022-05-22 18:31:22 +0545

    implemented and feed dynamic social economic data

--- components/mapbox/Map.js
+++ components/mapbox/Map.js
@@ -1,10 +1,29 @@
-import React, { useRef, useMemo } from 'react';
+import React, { useRef, useMemo,useContext } from 'react';
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup,Polyline } from 'react-leaflet';
 import styles from "./Map.module.scss";
+import edudata from '/public/static/edu.json';
+import fianancialdata from '/public/static/finan.json';
+import healthdata from '/public/static/health.json';
 
 
+import { FilterContext } from '../../context/FilterContext';
 const OsmMap = ({ center, draggable, onDragMarker, location }) => {
+    
+    const { state, dispatch } = useContext(FilterContext);
+    
+
+    const socioeconomic = state["socioeconomic"]["data"];
+    var health_care_institutions = socioeconomic.find(e => e.slug === 'health_care_institutions');
+    var health_care_institutions_status =health_care_institutions.status;
+
+    var financial_institutions = socioeconomic.find(e => e.slug === 'financial_institutions');
+    var financial_institutions_status =financial_institutions.status;
+
+    var educational_facilities = socioeconomic.find(e => e.slug === 'educational_facilities');
+    var educational_facilities_status =educational_facilities.status;
+
+
     const markerRef = useRef(null);
 
     const dragHandlers = useMemo(
@@ -27,10 +46,15 @@ const OsmMap = ({ center, draggable, onDragMarker, location }) => {
 
     var customIcon = new LeafIcon({ iconUrl: "/images/marker2.png" });
 
-
+    const redOptions = { color: 'red' }
+    const purpleOptions = { color: 'purple' }
+    const orangeOptions = { color: 'orange' }
+    
     return (
-        <MapContainer
-            center={[13.883084, 120.921398]}
+<>
+
+<MapContainer
+           center={[13.883084, 120.921398]}
             zoom={15}
             scrollWheelZone={true}
             className={styles.container}
@@ -38,7 +62,7 @@ const OsmMap = ({ center, draggable, onDragMarker, location }) => {
 
 
 
-            <TileLayer
+<TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
              //   url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/512/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
             
@@ -46,23 +70,23 @@ const OsmMap = ({ center, draggable, onDragMarker, location }) => {
 url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             />
 
-            <Marker
-                icon={customIcon}
-                position={
-                    [
-                        location && location.lng ? location.lng : "",
-                        location && location.lat ? location.lat : "",
-                    ]
-                }
-                draggable={draggable}
-                dragHandlers={dragHandlers}
-                ref={markerRef}
-            >
-                <Popup className={styles.popup}>
-                    {"my title"}
-                </Popup>
-            </Marker>
+{educational_facilities_status && edudata.features.map(edulibrary => (
+    <Polyline pathOptions={redOptions} positions={edulibrary.geometry.coordinates[0][0]} />
+            ))}
+
+{financial_institutions_status && fianancialdata.features.map(finanlibrary => (
+    <Polyline pathOptions={purpleOptions} positions={finanlibrary.geometry.coordinates[0][0]} />
+            ))}
+
+{health_care_institutions_status && healthdata.features.map(healthlibrary => (
+    <Polyline pathOptions={orangeOptions} positions={healthlibrary.geometry.coordinates[0][0]} />
+            ))}
+       
         </MapContainer>
+
+</>
+  
+
     )
 }
 export default OsmMap;
