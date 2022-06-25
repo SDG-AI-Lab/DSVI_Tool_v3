3bcbe8f80dae9aa17006e5d10dec045a5d127669
imsakararyal <imsakararyal@gmail.com>
2022-06-25 17:55:19 +0545

    sidebar fix,leveling,gradient tiles,hovering fix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -171,8 +171,8 @@ const Sidebar = () => {
                   >
 
 
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
                     <span>Select Level</span>
                   </span>
