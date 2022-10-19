764ec3be1b319d3cb7e7c6ca631ef6e6ae4a7ef7
andreiPro <avprokhorov1990@gmail.com>
2022-10-19 16:56:37 -0500

    FixedErrorsWithNamingOf_tooltipName_tooltipDescription

--- components/controls/MapControls.js
+++ components/controls/MapControls.js
@@ -10,11 +10,11 @@ const ControlButton = (props) => {
             src={props.src}
             alt={props.alt}
             className={`transition ease-in-out delay-150 hover:scale-110 hover:bg-white-500 duration-300 bg-white cursor-pointer border-blue-600 border-2 p-1 h-11 w-11 bg-opacity-75 ${props.className}`}
-            tooltipName="Dummy hover text"
-            tooltipDescription="Some dummy text here."
+            tooltip_name="Dummy hover text"
+            tooltip_description="Some dummy text here."
         />
         <div className="invisible absolute top-0 right-32 bg-white rounded-md px-2 py-2 absolute text-black text-white w-64 group-hover:visible">
-            <strong>{props.tooltipName}</strong> - <span>{props.tooltipDescription}</span></div>
+            <strong>{props.tooltipname}</strong> - <span>{props.tooltip_description}</span></div>
     </div>
 }
 
@@ -25,15 +25,15 @@ const MapControls = (props) => {
         <ControlButton
             src="images/country-icon.7a31d42f.png"
             alt="contry selector"
-            tooltipName="Dummy hover text"
-            tooltipDescription="Some dummy text here."
+            tooltipname="Dummy hover text"
+            tooltip_description="Some dummy text here."
         />
         <ControlButton
             src="images/add-boundaries-icon.409e70b2.png"
             alt="Boundaries"
             className="ml-2"
-            tooltipName="Dummy hover text"
-            tooltipDescription="Some dummy text here."
+            tooltipname="Dummy hover text"
+            tooltip_description="Some dummy text here."
         />
     </Control>
 }
