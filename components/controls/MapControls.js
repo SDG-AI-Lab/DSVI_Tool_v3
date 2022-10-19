5b98fd289715d9c5f4c660d033a77d0f4ec8828e
Martin Szigeti <38685413+Skerre@users.noreply.github.com>
2022-10-19 15:36:52 +0200

    Revert "Enable hover text to the top two buttons on the Control Menu"

--- components/controls/MapControls.js
+++ components/controls/MapControls.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Control from 'react-leaflet-custom-control'
-import { Tooltip } from 'react-leaflet'
-
+import { Tooltip } from "react-leaflet"
 
 const ControlButton = (props) => {
 
@@ -10,8 +9,6 @@ const ControlButton = (props) => {
             src={props.src}
             alt={props.alt}
             className={`transition ease-in-out delay-150 hover:scale-110 hover:bg-white-500 duration-300 bg-white cursor-pointer border-blue-600 border-2 p-1 h-11 w-11 bg-opacity-75 ${props.className}`}
-            tooltipName="Dummy hover text"
-            tooltipDescription="Some dummy text here."
         />
         <div className="invisible absolute top-0 right-32 bg-white rounded-md px-2 py-2 absolute text-black text-white w-64 group-hover:visible">
             <strong>{props.tooltipName}</strong> - <span>{props.tooltipDescription}</span></div>
@@ -20,8 +17,8 @@ const ControlButton = (props) => {
 
 const MapControls = (props) => {
 
-    return <Control position={props.position} style={{ display: "flex" }}>
-
+    return <Control position={props.position} style={{display: "flex"}}>
+       
         <ControlButton
             src="images/country-icon.7a31d42f.png"
             alt="contry selector"
