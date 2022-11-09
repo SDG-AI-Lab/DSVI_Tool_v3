a075a012aabdd8c059ee0c997d899a962311f4b4
Martin Szigeti <38685413+Skerre@users.noreply.github.com>
2022-11-09 13:26:41 +0300

    changed the name of the reset button

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -121,7 +121,7 @@ const Sidebar = (props) => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                   </svg>
-                  <span>{reset_settings == false ? 'Reset Settings(false)' : 'Reset Settings(true)'}</span>
+                  <span>{reset_settings == false ? 'Reset Tool' : 'Reset Tool'}</span>
                   </a>
                 </li>
 
