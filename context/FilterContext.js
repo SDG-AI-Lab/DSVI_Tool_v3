f83cd94ac20c5e78ecdea2bdaf838530231c90a1
andreiPro <avprokhorov1990@gmail.com>
2022-11-08 23:01:27 -0600

    beta-reset-settings-button

--- context/FilterContext.js
+++ context/FilterContext.js
@@ -1,10 +1,10 @@
 import React, {useReducer} from 'react'
-import {reducer, initalState} from '../reducer/reducer'
+import {reducer, initialState} from '../reducer/reducer'
 
 export const FilterContext = React.createContext()
 
 export const FilterProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initalState)
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (<FilterContext.Provider value={{state, dispatch}}>
             {children}
         </FilterContext.Provider>)
--- reducer/legendReducer.js
+++ reducer/legendReducer.js
@@ -1,4 +1,4 @@
-export const initalState = {
+export const initialState = {
     "title": "", "description": "", "data": []
 }
 
