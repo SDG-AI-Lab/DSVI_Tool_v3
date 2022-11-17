97d706c370ca81c992b52e8baf99ac5a2c595a93
Martin Szigeti <38685413+Skerre@users.noreply.github.com>
2022-11-17 17:51:46 +0300

    infobox: Added content, changed size, layout, text justify, New image for Social Vulnerability

--- components/controls/InfoBox.js
+++ components/controls/InfoBox.js
@@ -10,22 +10,22 @@ import L from 'leaflet';
 
 // options for the drop down menu in infoBox
 const dropDownOptions = [
-  'Select One', 'SV: Random Forest', 'SV: XGBoost', 'DT: Education Facility', 'DT: Health Institution', 'DT: Financial Service',
+  'Social Vulnerability', 'SV Prediction: Random Forest', 'SV: Prediction: XGBoost', 'Accessibility: Education Facility', 'Accessibility: Health Institution', 'Accessibility: Financial Service',
   'Population Counts', 'Celltowers', 'Nightlight Intensity', 'Relative Wealth', 'GDP', 'Plant Health', 'Temperature (Max)',
   'Land Use Class', 'Elevation'
 ]
 
 // each index for the above list has a description in the list below which is shown in infoBox
 const dropDownDescriptions = [
-  {heading: 'Social Vulnerability Platform', desc: 'Hello and welcome to the DSVI Tool! This tool visualizes Social Vulnerability and Data Relevant for Social Vulnerability in Tajikistan. This is the info box.'},
-  {heading: 'SV: Random Forest', desc: 'This is about forests'},
-  {heading: 'SV: XGBoost', desc: 'This is about boosting XG'},
-  {heading: 'DT: Education Facility', desc: 'This is about forests'},
-  {heading: 'DT: Health Institution', desc: 'This is about boosting XG'},
-  {heading: 'DT: Financial Service', desc: 'This is about forests'},
-  {heading: 'Population Counts', desc: 'This is about boosting XG'},
-  {heading: 'Celltowers', desc: 'This is about forests'},
-  {heading: 'Nightlight Intensity', desc: 'This is about boosting XG'},
+  {heading: 'Social Vulnerability Platform', desc: 'Social Vulnerability (SV) is the capacity of individuals or communities to cope with social and environmental shocks   (Adger 2000, Cutter 2003). This includes climate change, natural disasters, and other societal risks. Vulnerable groups have a disproportionate risk of being affected and experiencing more profound consequences, due to their socio-economic preconditions. SV assessments help to better map the connection between local conditions, social characteristics, or individual vulnerabilities and risks. The calculation of SV scores is a frequent practice to measure a community’s ability to respond to outside stressors and risks. It is an indirect way to quantify resilience. Having such an assessment helps to understand, get prepared and respond in a more effective manner, using a combination of the most appropriate tools once the risk materializes.'},
+  {heading: 'SV Prediction: Random Forest', desc: 'This layer is a prediction of Social Vulnerability with sklearns Random Forest Regressor. A random forest is a meta estimator that fits a number of classifying decision trees on various sub-samples of the dataset and uses averaging to improve the predictive accuracy and control over-fitting. The sub-sample size is controlled with the max_samples parameter if bootstrap=True (default), otherwise the whole dataset is used to build each tree.'},
+  {heading: 'SV: Prediction: XGBoost', desc: 'This algorithm builds an additive model in a forward stage-wise fashion; it allows for the optimization of arbitrary differentiable loss functions. In each stage n_classes_ regression trees are fit on the negative gradient of the loss function, e.g. binary or multiclass log loss. Binary classification is a special case where only a single regression tree is induced.'},
+  {heading: 'Accessibility: Education Facility', desc: 'This layer shows the time it takes to drive an individual to the next available Education Facility. The data was provided by OpenStreetMap. The download source is humdata.org'},
+  {heading: 'Accessibility: Health Institution', desc: 'This layer shows the time it takes to drive an individual to the next available Health Facility. The data was provided by OpenStreetMap. The download source is humdata.org'},
+  {heading: 'Accessibility: Financial Service', desc: 'This layer shows the time it takes to drive an individual to the next available Financial Facility. The data was provided by OpenStreetMap. The download source is humdata.org'},
+  {heading: 'Population Counts', desc: 'The population data is by: https://sedac.ciesin.columbia.edu/data/collection/gpw-v4'},
+  {heading: 'Celltowers', desc: 'Celltower data is by: '},
+  {heading: 'Nightlight Intensity', desc: 'Nightlight Intensity'},
   {heading: 'Relative Wealth', desc: 'This is about forests'},
   {heading: 'GDP', desc: 'This is about boosting XG'},
   {heading: 'Plant Health', desc: 'This is about forests'},
@@ -84,6 +84,7 @@ const infoBoxRef = useRef();
         {show_infoBox_data ?
           <div>
             <h2 className='font-bold text-xl mb-3 ml-3'>Information Panel</h2>
+            <hr className= 'mb-3'></hr>
             <button className="button-infoBox" onClick={(e) => {
               e.stopPropagation();
               e.preventDefault();
@@ -97,7 +98,7 @@ const infoBoxRef = useRef();
               placeholder="Select an option" />
             <div className='max-w-md px-4 mt-5'>
               <h2 className='font-bold'>{dropDownDescriptions[dropdownDescIndex].heading}</h2>
-              <p className='my-3'>{dropDownDescriptions[dropdownDescIndex].desc}</p>
+              <p className='my-3 leading-relaxed mb-3 text-justify'>{dropDownDescriptions[dropdownDescIndex].desc}</p>
             </div>
           </div>
         : null }
@@ -116,13 +117,13 @@ const Tabs = () => {
    
       <div className="flex items-start mb-3">
         <ul
-          className="nav nav-tabs mr-4 flex list-none flex-col flex-wrap border-b-0 pl-0"
+          className="nav nav-tabs mr-1 flex list-none flex-col flex-wrap border-b-0 pl-0"
           role="tablist"
         >
           <li
             className="nav-item flex-grow text-left"
             style={{
-              background: openTab === 1 ? '#9d969659' : 'transparent',
+              background: openTab === 1 ? '#e6f9ff' : 'transparent',
             }}
           >
             <a
@@ -143,7 +144,7 @@ const Tabs = () => {
           <li
             className="nav-item flex-grow text-left"
             style={{
-              background: openTab === 2 ? '#9d969659' : 'transparent',
+              background: openTab === 2 ? '#e6f9ff' : 'transparent',
             }}
           >
             <a
@@ -164,7 +165,7 @@ const Tabs = () => {
           <li
             className="nav-item flex-grow text-left"
             style={{
-              background: openTab === 3 ? '#9d969659' : 'transparent',
+              background: openTab === 3 ? '#e6f9ff' : 'transparent',
             }}
           >
             <a
@@ -184,26 +185,26 @@ const Tabs = () => {
           </li>
         </ul>
         <div className="tab-content">
-          <div className="flex-auto px-2 py-0">
+          <div className="flex-auto ml-20">
             <div
               className={`${openTab === 1 ? 'block' : 'hidden'
                 } tab-pane fade show active`}
-              style={{ width: '200px' }}
+              style={{ width: '150px' }}
               id="link1"
             >
               <Carousel
-                className="info_carousel ml-3 px-2"
-                showArrows={true}
+                className="info_carousel"
+                showArrows={false}
                 showIndicators={false}
                 showThumbs={false}
                 showStatus={false}
               >
                 <img
-                  style={{ width: '100px' }}
-                  src="https://knowsdgs.jrc.ec.europa.eu/themes/sdgs/assets/img/sdg1.png"
+                  style={{ width: '100px'}}
+                  src="./images/logo-sdg-filled.png"
                   alt="poverty"
                 />
-                <img
+                {/* <img
                   style={{ width: '100px' }}
                   src="https://knowsdgs.jrc.ec.europa.eu/themes/sdgs/assets/img/sdg2.png"
                   alt="poverty"
@@ -217,13 +218,13 @@ const Tabs = () => {
                   style={{ width: '100px' }}
                   src="https://knowsdgs.jrc.ec.europa.eu/themes/sdgs/assets/img/sdg4.png"
                   alt="poverty"
-                />
+                /> */}
               </Carousel>
             </div>
             <div
               className={`${openTab === 2 ? 'block' : 'hidden'
                 } tab-pane fade show active`}
-              style={{ width: '150px' }}
+              style={{ width: '200px' }}
               id="link2"
             >
               <Carousel
