da90c7ed4d934b74ceeeee6f36750462892a4fd4
Martin Szigeti <38685413+Skerre@users.noreply.github.com>
2022-11-27 18:54:59 +0300

    Volunteer Page back

--- pages/about.jsx
+++ pages/about.jsx
@@ -27,13 +27,13 @@ const Home = () => {
             })
           } 
 
-          {/* <p className="py-5 px-5 ">
+          <p className="py-5 px-5 ">
             <a href="/volunteer" class="volunteer-link" target="_blank">
               <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
               United Nations Online Volunteers            
               </button>
             </a>
-          </p>  */}
+          </p> 
 
         </div>        
       </div>
