b36e92e9384dcf366ed170bec576e7026b0ac37f
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-17 21:49:52 -0500

    Extracted SVGs into components SVG.tsx

---
+++ components/SVGs.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export const BottleIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="mr-2 h-4 w-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+      />
+    </svg>
+  )
+}
+
+export const DollarIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="mr-2 h-4 w-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11
+    0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
+
+export const ResetIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="mr-2 h-4 w-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+      />
+    </svg>
+  )
+}
+
+export const HeartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="mr-2 h-4 w-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+      />
+    </svg>
+  )
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,6 +3,7 @@ import { Modal } from 'react-responsive-modal'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { FilterContext } from '../context/FilterContext'
 import DHS_COLUMN from './../public/static/color_gradient_lookup.json'
+import { BottleIcon, DollarIcon, HeartIcon, ResetIcon } from './SVGs'
 
 const Sidebar = (props) => {
   const { show_infoBox_data } = props
@@ -73,7 +74,7 @@ const Sidebar = (props) => {
         className={`h-[calc(100vh-110px)] w-[330px] min-w-[100px] max-w-[500px] resize-x overflow-auto bg-white`}
       >
         <div id="sidenavSecExample">
-          <div></div>
+          {/* <div></div> */}
           <hr className="my-0" />
           <div>
             <div className="flex h-8 items-center justify-center">
@@ -104,20 +105,7 @@ const Sidebar = (props) => {
                       )
                     }}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mr-2 h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                      />
-                    </svg>
+                    <ResetIcon />
                     <span>
                       {reset_settings == false ? 'Reset Tool' : 'Reset Tool'}
                     </span>
@@ -142,20 +130,7 @@ const Sidebar = (props) => {
                       color: areaofInterestStatus2 ? 'rgb(37 99 235)' : null,
                     }}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mr-2 h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                      />
-                    </svg>
+                    <HeartIcon />
                     <span>
                       {areaofInterestStatus2 == true
                         ? 'Hide Area of Interest'
@@ -177,20 +152,7 @@ const Sidebar = (props) => {
                         dispatch({ type: 'TOGGLE_VULNERABILITY', payload: {} })
                       }
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="mr-2 h-4 w-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-                        />
-                      </svg>
+                      <BottleIcon />
                       <span>
                         {vulnerability == true
                           ? 'Hide Vulnerability'
@@ -404,21 +366,7 @@ const Sidebar = (props) => {
                     aria-controls="collapseSidenavSecEx2"
                     onClick={() => dispatch({ type: 'TOGGLE_SOCIOECONOMIC' })}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mr-2 h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11
-                        0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
+                    <DollarIcon />
                     <span>Socioeconomic Layers</span>
                     {socioeconomic.status == true ? (
                       <svg
@@ -1046,13 +994,7 @@ const Sidebar = (props) => {
         </div>
       </div>
 
-      <Modal
-        open={dsvModal}
-        onClose={() => {
-          onCloseDsvModal()
-        }}
-        center
-      >
+      <Modal open={dsvModal} onClose={() => onCloseDsvModal()} center>
         <div></div>
 
         <div className="my-2 max-w-md overflow-y-auto rounded-lg bg-white py-4 px-8 shadow-lg">
