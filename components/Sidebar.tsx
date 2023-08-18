6953912bc469d87b6f7829ca96a3df6ee986bf53
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-17 23:41:30 -0500

    Extracted Admin Levels into AdminLevel.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import {
   ResetIcon,
 } from './SVGs'
 import SidebarToggle from './SidebarToggle'
+import AdminLevels from './AdminLevels'
 
 const Sidebar = (props) => {
   const { show_infoBox_data } = props
@@ -363,59 +364,7 @@ const Sidebar = (props) => {
                       <AdministrativeIcon />
                       <span>Select administrative level</span>
                     </span>
-                    <ul className="flex">
-                      {[
-                        {
-                          slug: 1,
-                          title: 'One',
-                        },
-                        {
-                          slug: 2,
-                          title: 'Two',
-                        },
-                        {
-                          slug: 3,
-                          title: 'Three',
-                        },
-                      ].map((val, index) => {
-                        return (
-                          <div key={index}>
-                            <div
-                              className="flex"
-                              onClick={() => {
-                                dispatch({
-                                  type: 'CHANGE_LEVEL',
-                                  payload: { level: val.slug },
-                                })
-                              }}
-                            >
-                              <input
-                                className="focus:ring-3  ml-5 h-4 w-4 rounded border-gray-300 bg-gray-50 focus:ring-blue-300"
-                                id="flowbite"
-                                aria-describedby="flowbite"
-                                type="radio"
-                                checked={val.slug == level ? true : false}
-                                onChange={() =>
-                                  dispatch({
-                                    type: 'CHANGE_LEVEL',
-                                    payload: { level: val.slug },
-                                  })
-                                }
-                              />
-                              <a
-                                href="#!"
-                                className="flex h-6 items-center  overflow-hidden  text-ellipsis whitespace-nowrap rounded pl-2 text-xs text-gray-700 transition duration-300 ease-in-out hover:bg-blue-50 hover:text-blue-600"
-                                data-mdb-ripple="true"
-                                data-mdb-ripple-color="primary"
-                              >
-                                {val.title}
-                              </a>
-                            </div>
-                            <div className="text-gray-700"></div>
-                          </div>
-                        )
-                      })}
-                    </ul>
+                    <AdminLevels />
                   </div>
                 )}
                 {/* {socioeconomic.status == true &&
