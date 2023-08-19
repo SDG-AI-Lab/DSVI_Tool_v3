a23cec1b1ae9fc027e76f4dd8c10897ef7115ab9
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-18 23:09:39 -0500

    Refactor and cleanup of OpacityRange.tsx

--- components/OpacityRange.tsx
+++ components/OpacityRange.tsx
@@ -32,23 +32,19 @@ function OpacityRange({
   const inputClassNames =
     'input-sm mx-2 w-14 border rounded border-solid border-gray-300 bg-white bg-clip-padding text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
 
-  const onClickChange = (
-    val2,
-    index: number,
-    index2: number,
-    event?: ChangeEvent<HTMLInputElement>
-  ): void => {
+  const onClickChange = (event: ChangeEvent<HTMLInputElement>): void => {
     let value = event ? event.target.value : val2.value
+    const { id, slug, title, reverse_meaning, units, json_library } = val2
 
     const newItem = {
-      id: val2.id,
-      slug: val2.slug,
-      title: val2.title,
+      id,
+      slug,
+      title,
       status: !val2.status,
       value,
-      reverse_meaning: val2.reverse_meaning,
-      units: val2.units,
-      json_library: val2.json_library,
+      reverse_meaning,
+      units,
+      json_library,
     }
     dispatch({
       type: 'CHANGE_SOCIOECONOMIC',
@@ -56,19 +52,9 @@ function OpacityRange({
       index_1: index,
       index_2: index2,
     })
-    if (event) return
-    dispatch({
-      type: 'CHANGE_ACTIVE_LEGENDS',
-      payload: newItem,
-    })
   }
 
-  const onRangeChange = (
-    val2: val2,
-    index: number,
-    index2: number,
-    range: string
-  ) => {
+  const onRangeChange = (range: string) => {
     const newItem = {
       id: val2.id,
       slug: val2.slug,
@@ -92,17 +78,15 @@ function OpacityRange({
 
   useEffect(() => {
     const id = setTimeout(() => {
-      // console.log('setting new time out')
       setDebouncedRange(range)
     }, delay)
 
     return () => {
-      // console.log('clearing the timeout')
       clearTimeout(id)
     }
   }, [range])
   useEffect(() => {
-    onRangeChange(val2, index, index2, debouncedRange)
+    onRangeChange(debouncedRange)
   }, [debouncedRange])
 
   return (
@@ -115,14 +99,13 @@ function OpacityRange({
           type="number"
           className={inputClassNames}
           value={parseInt(val2.value)}
-          onChange={(e) => onClickChange(val2, index, index2, e)}
+          onChange={(e) => onClickChange(e)}
         />
         <input
           type="range"
           min="0"
           max="100"
           step="1"
-          // value={val2.value}
           value={range}
           className="form-range h-6 p-0 focus:shadow-none focus:outline-none focus:ring-0"
           onChange={(e) => setRange(e.target.value)}
