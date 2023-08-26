8adcc1fb932aa369fbe673d503a60945d0e6a3fe
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-26 11:10:08 -0500

    SocioeconLayers.tsx moved comments, removed unnesessary non-functional lines

--- components/sidebar/SocioeconLayers.tsx
+++ components/sidebar/SocioeconLayers.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, ChangeEvent } from 'react'
+import React, { useContext } from 'react'
 import { FilterContext } from '../../context/FilterContext'
 import {
   DollarIcon,
@@ -16,9 +16,6 @@ function SocioeconLayers() {
 
   const classNames =
     'flex items-center overflow-hidden text-ellipsis whitespace-nowrap rounded py-4 text-gray-700 transition duration-300 ease-in-out hover:bg-blue-50 hover:text-blue-600'
-
-  const inputClassNames =
-    'input-sm mx-2 w-14 border rounded border-solid border-gray-300 bg-white bg-clip-padding text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none'
 
   const onChange = (val2, index: number, index2: number): void => {
     const newItem = {
@@ -48,12 +45,6 @@ function SocioeconLayers() {
     <div className="relative" id="sidenavSecEx3">
       <a
         className={`h-12 cursor-pointer px-2 text-sm ${classNames}`}
-        data-mdb-ripple="true"
-        data-mdb-ripple-color="primary"
-        data-bs-toggle="collapse"
-        data-bs-target="#collapseSidenavSecEx2"
-        aria-expanded="false"
-        aria-controls="collapseSidenavSecEx2"
         onClick={() => dispatch({ type: 'TOGGLE_SOCIOECONOMIC' })}
       >
         <DollarIcon />
@@ -73,39 +64,7 @@ function SocioeconLayers() {
           <AdminLevels />
         </div>
       )}
-
-      {/* {socioeconomic.status == true &&
-          <ul className="flex">
-            {
-              [{
-                slug: '_sum',
-                title: 'Sum'
-              },
-              {
-                slug: '_count',
-                title: 'Count'
-              },
-              {
-                slug: '_avg',
-                title: 'AVG'
-              }].map((val, index) => {
-                return (
-                  <div key={index}>
-                    <div className="flex" onClick={ () => { dispatch({ type: "CHANGE_SOCIOECONOMIC_DATA_COLUMN", payload: val.slug }) }}>
-                      <input className="ml-5  bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded" type="radio"
-                        checked={val.slug == socioeconomic.data_column ? true : false}
-                        onChange={() => {dispatch({ type: "CHANGE_SOCIOECONOMIC_DATA_COLUMN", payload: val.slug })}}
-                      />
-                      <a href="#!" className="flex items-center text-xs  pl-2  h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="primary">{val.title}</a>
-                    </div>
-                    <div className="text-gray-700">
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </ul>
-        } */}
+      {/* COMMENT #1 (AT THE BOTTOM OF THE FILE) WAS HERE */}
 
       <ul
         className="accordion-collapse collapse relative"
@@ -179,55 +138,7 @@ function SocioeconLayers() {
                                   delay={100}
                                   isVisible={Boolean(val2.status)}
                                 />
-                                {/* {val2.status && (  
-                                      <div className="flex flex-col space-y-2 p-2 px-6">
-                                        <span className="text-sm text-gray-700">
-                                          opacity:
-                                          <input
-                                            type="number"
-                                            className={inputClassNames}
-                                            value={parseInt(val2.value)}
-                                            onChange={(e) =>
-                                              onClickChange(
-                                                val2,
-                                                index,
-                                                index2,
-                                                e
-                                              )
-                                            }
-                                          />
-                                          <input
-                                            type="range"
-                                            min="0"
-                                            max="100"
-                                            step="5"
-                                            value={val2.value}
-                                            className="form-range h-6 p-0
-                                                  focus:shadow-none focus:outline-none focus:ring-0"
-                                            onChange={(event) => {
-                                              console.log(event.target.value)
-                                              const newItem = {
-                                                id: val2.id,
-                                                slug: val2.slug,
-                                                title: val2.title,
-                                                status: val2.status,
-                                                value: event.target.value,
-                                                reverse_meaning:
-                                                  val2.reverse_meaning,
-                                                units: val2.units,
-                                                json_library: val2.json_library,
-                                              }
-                                              dispatch({
-                                                type: 'CHANGE_SOCIOECONOMIC',
-                                                payload: newItem,
-                                                index_1: index,
-                                                index_2: index2,
-                                              })
-                                            }}
-                                          />
-                                        </span>
-                                      </div>
-                                    )} */}
+                                {/* COMMENT #2 (AT THE BOTTOM OF THE FILE) WAS HERE */}
                               </li>
                             )}
                           </Draggable>
@@ -245,3 +156,92 @@ function SocioeconLayers() {
 }
 
 export default SocioeconLayers
+
+// COMMENT #1:
+{
+  /* {socioeconomic.status == true &&
+    <ul className="flex">
+      {
+        [{
+          slug: '_sum',
+          title: 'Sum'
+        },
+        {
+          slug: '_count',
+          title: 'Count'
+        },
+        {
+          slug: '_avg',
+          title: 'AVG'
+        }].map((val, index) => {
+          return (
+            <div key={index}>
+              <div className="flex" onClick={ () => { dispatch({ type: "CHANGE_SOCIOECONOMIC_DATA_COLUMN", payload: val.slug }) }}>
+                <input className="ml-5  bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded" type="radio"
+                  checked={val.slug == socioeconomic.data_column ? true : false}
+                  onChange={() => {dispatch({ type: "CHANGE_SOCIOECONOMIC_DATA_COLUMN", payload: val.slug })}}
+                />
+                <a href="#!" className="flex items-center text-xs  pl-2  h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="primary">{val.title}</a>
+              </div>
+              <div className="text-gray-700">
+              </div>
+            </div>
+          )
+        })
+      }
+    </ul>
+  } */
+}
+
+// COMMENT #2:
+{
+  /* {val2.status && (  
+  <div className="flex flex-col space-y-2 p-2 px-6">
+        <span className="text-sm text-gray-700">
+          opacity:
+          <input
+            type="number"
+            className={inputClassNames}
+            value={parseInt(val2.value)}
+            onChange={(e) =>
+              onClickChange(
+                val2,
+                index,
+                index2,
+                e
+              )
+            }
+          />
+          <input
+            type="range"
+            min="0"
+            max="100"
+            step="5"
+            value={val2.value}
+            className="form-range h-6 p-0
+                  focus:shadow-none focus:outline-none focus:ring-0"
+            onChange={(event) => {
+              console.log(event.target.value)
+              const newItem = {
+                id: val2.id,
+                slug: val2.slug,
+                title: val2.title,
+                status: val2.status,
+                value: event.target.value,
+                reverse_meaning:
+                  val2.reverse_meaning,
+                units: val2.units,
+                json_library: val2.json_library,
+              }
+              dispatch({
+                type: 'CHANGE_SOCIOECONOMIC',
+                payload: newItem,
+                index_1: index,
+                index_2: index2,
+              })
+            }}
+          />
+        </span>
+      </div>
+    )} */
+}
