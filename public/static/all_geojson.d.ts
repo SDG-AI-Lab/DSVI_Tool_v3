d696f894eb5a4cef24cd0e66c2fa3f008f51a8cd
Vadim Fattakhov <vadfthv@gmail.com>
2023-10-13 19:28:13 -0500

    Fixed TypeScript not recognizing .geojson

--- public/static/all_geojson.d.ts
+++ public/static/all_geojson.d.ts
@@ -20,182 +20,182 @@ type GeoJsonFeaturesType = {
   }
 }
 
-export type GeoJsonType = {
+interface SingleGeoJson {
   type: string
   name: string
   crs: any
-  features: GeoJsonFeaturesType[]
+  features: GeoJsonFeaturesType
 }
 declare module 'public/static/rf_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 
 declare module 'public/static/rf_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/rf_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/XGBoost_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/XGBoost_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/XGBoost_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/edu_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/edu_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/edu_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/health_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/health_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/health_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/finan_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/finan_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/finan_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ppp_pop_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ppp_pop_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ppp_pop_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/cellt_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/cellt_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/cellt_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ntl_vnl_npp_2016_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ntl_vnl_npp_2016_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ntl_vnl_npp_2016_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/rwi_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/rwi_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/rwi_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/gdp_2015_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/gdp_2015_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/gdp_2015_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ndvi_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ndvi_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/ndvi_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/temp_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/temp_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/temp_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/lu_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/lu_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/lu_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/dem_1.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/dem_2.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/dem_3.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
 declare module 'public/static/AOI.geojson' {
-  const singleGeojson: GeoJsonType
+  const singleGeojson: SingleGeoJson
   export default singleGeojson
 }
