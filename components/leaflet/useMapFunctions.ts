2eb98a73192127cf21065ec12f0422ca75b349b3
Vadim Fattakhov <vadfthv@gmail.com>
2023-10-18 15:38:10 -0500

    Cleanup in useMapfFunctions.tsx, renamed it to .ts since no jsx in it

--- components/leaflet/useMapFunctions.ts
+++ components/leaflet/useMapFunctions.ts
@@ -1,6 +1,3 @@
-import React from 'react'
-import CustomPolygon from '../controls/CustomPolygon'
-import L from 'leaflet'
 import {
   ReducerInitialStateType,
   SvLayerObjectType,
@@ -9,133 +6,12 @@ import { SeLayerObjectType } from '../../reducer/reducerInitialState'
 
 import { geojson } from 'public/static'
 
-interface CombinedLayerData {
+type CombinedLayerData = {
   layerInfo: SeLayerObjectType | undefined
   geojson: SingleGeoJson[]
 }
 
 export const useMapFunctions = () => {
-  // const mapPolygonColorToDensity = (
-  //   normalizeData: number,
-  //   layerObject: SeLayerObjectType
-  // ): string => {
-  //   if (!layerObject.reverse_meaning) {
-  //     switch (true) {
-  //       case normalizeData > 0.8 && normalizeData <= 1:
-  //         return '#FF362C' // RED
-  //       case normalizeData > 0.6:
-  //         return '#ff962c' // ORANGE
-  //       case normalizeData > 0.4:
-  //         return '#FFDE2C' // YELLOW
-  //       case normalizeData > 0.2:
-  //         return '#00800A' // GREEN
-  //       case normalizeData >= 0:
-  //         return '#0c58ca' // BLUE
-  //       default:
-  //         return '#FFFFFF' // WHITE
-  //     }
-  //   } else {
-  //     switch (true) {
-  //       case normalizeData > 0.8 && normalizeData <= 1:
-  //         return '#0c58ca' // BLUE
-  //       case normalizeData > 0.6:
-  //         return '#00800A' // GREEN
-  //       case normalizeData > 0.4:
-  //         return '#FFDE2C' // YELLOW
-  //       case normalizeData > 0.2:
-  //         return '#ff962c' // ORANGE
-  //       case normalizeData >= 0:
-  //         return '#FF362C' // RED
-  //       default:
-  //         return '#FFFFFF' // WHITE
-  //     }
-  //   }
-  // }
-
-  // const NewProjection = (
-  //   full_JSON_library,
-  //   library,
-  //   index: number,
-  //   layerObject: SeLayerObjectType,
-  //   show_data: boolean
-  // ): JSX.Element => {
-  //   const {
-  //     NAME,
-  //     NAME_1,
-  //     NAME_2,
-  //     _mean,
-  //     _count,
-  //     _stdev,
-  //     _max,
-  //     _min,
-  //     _sum,
-  //     _avg,
-  //   } = library.properties
-  //   const {} = library.name
-  //   const data = [
-  //     // These are shown when the user clicks on the polygon
-  //     {
-  //       key: 'Oblast',
-  //       value: NAME_1,
-  //     },
-  //     {
-  //       key: 'District',
-  //       value: NAME_2,
-  //     },
-  //     {
-  //       key: 'Count',
-  //       value: _count,
-  //     },
-  //     {
-  //       key: 'Mean value',
-  //       value: _mean,
-  //     },
-  //   ]
-  //   const onlyAllMeanNumbers = full_JSON_library.features.map(
-  //     (object) => object.properties._mean
-  //   )
-  //   // console.log('onlyAllMeanNumbers');
-  //   // console.log(onlyAllMeanNumbers);
-  //   const minMeanNumber = Math.min(...onlyAllMeanNumbers)
-  //   const maxMeanNumber = Math.max(...onlyAllMeanNumbers)
-
-  //   const normalizeDataValue = Math.abs(
-  //     (_mean - minMeanNumber) / (maxMeanNumber - minMeanNumber)
-  //   )
-  //   const fillColor = mapPolygonColorToDensity(normalizeDataValue, layerObject)
-
-  //   return (
-  //     <CustomPolygon
-  //       key={index}
-  //       positions={L.GeoJSON.coordsToLatLngs(
-  //         library.geometry.coordinates[0][0]
-  //       )}
-  //       fillColor={fillColor}
-  //       hoverColor={fillColor}
-  //       opacity={layerObject.value / 100}
-  //       tooltipDirection="auto"
-  //       tooltipOffset={[20, 0]}
-  //       tooltipCount={library.properties._mean.toFixed(2)} // library.properties._count
-  //       normalizeDataValue={normalizeDataValue.toFixed(2)}
-  //       units={layerObject.units}
-  //       _mean={_mean.toFixed(2)}
-  //       minMeanNumber={minMeanNumber.toFixed(2)}
-  //       maxMeanNumber={maxMeanNumber.toFixed(2)}
-  //       tooltipName_1={library.properties.NAME_1}
-  //       tooltipName_2={library.properties.NAME_2}
-  //       tooltipName_3={library.properties.NAME_2}
-  //       tooltipBgcolor="rgb(255 255 255)"
-  //       tooltipTextColor="text-slate-700"
-  //       show_data={show_data}
-  //       popupMaxWidth="auto"
-  //       popupMaxHeight="auto"
-  //       popupBgColor="rgb(255 255 255)"
-  //       popupTextColor="text-slate-700"
-  //       data={data}
-  //     />
-  //   )
-  // }
-
   const seLayersData = (state: ReducerInitialStateType) => {
     const se_xgboost = state.socioeconomic.data
       .find((x) => x.slug === 'se_social_vulnerability')
@@ -363,18 +239,15 @@ export const useMapFunctions = () => {
     return [
       sv_xgboost,
       sv_random_forest,
-      // distance_maps,
       distance_to_healthcare,
       distance_to_finance,
       distance_to_edu,
       roads,
-      // bio_physical,
       elevation,
       slope,
       max_temp,
       plant_health,
       precipitation,
-      // socio_economic,
       nightlight_intensity,
       pop_density,
       celltower,
@@ -411,7 +284,6 @@ export const useMapFunctions = () => {
   }
 
   return {
-    // NewProjection,
     seLayersData,
     svLayersData,
     categoriesData,
