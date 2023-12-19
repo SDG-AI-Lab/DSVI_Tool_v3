a1060f1e473f084539490540f619970489220b55
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-18 21:13:02 -0600

    Basic AithContext.tsx

---
+++ context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react'
+
+export const AuthContext = createContext(undefined)
+
+export const AuthProvider = ({ children }) => {
+  const initialState = {
+    isLoading: false,
+    user: null,
+    error: null,
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'REGISTER_USER_PENDING':
+        return { ...state, isLoading: true }
+      case 'REGISTER_USER_FULFILLED':
+        return { ...state, isLoading: false, user: action.payload }
+      case 'REGISTER_USER_REJECTED':
+        return { ...state, isLoading: false, error: action.error.message }
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import '../styles/globals.css'
 import Main from '../components/layout/Main'
 import { FilterProvider } from '../context/FilterContext'
 import { LegendProvider } from '../context/LegendContext'
+import { AuthProvider } from '../context/AuthContext'
 function MyApp({ Component, pageProps }) {
   return (
-    <FilterProvider>
-      <LegendProvider>
-        <Main>
-          <Component {...pageProps} />
-        </Main>
-      </LegendProvider>
-    </FilterProvider>
+    <AuthProvider>
+      <FilterProvider>
+        <LegendProvider>
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+        </LegendProvider>
+      </FilterProvider>
+    </AuthProvider>
   )
 }
 export default MyApp
