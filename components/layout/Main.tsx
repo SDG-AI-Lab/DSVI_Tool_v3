128f25e13ccacccf6ba5b7b7f021e4d6d85fb751
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-23 18:23:27 -0600

    Removed hamepage logic from Main.tsx

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -6,22 +6,20 @@ import { AuthContext } from '../../context/AuthContext'
 import { useRouter } from 'next/router'
 
 const layout = ({ children }) => {
-  const { state, dispatch } = useContext(FilterContext)
-  const on_homepage = state['on_homepage']
-
   const { state: authState } = useContext(AuthContext)
   const router = useRouter()
+
   useEffect(() => {
     if (typeof window !== 'undefined' && !authState.user) {
       router.push('/landing')
     }
-  }, [])
+  }, [authState.user])
 
   return (
     <div className="min-h-screen bg-slate-200">
-      {!on_homepage && <Header />}
+      <Header />
       {children}
-      {!on_homepage && <Footer />}
+      <Footer />
     </div>
   )
 }
