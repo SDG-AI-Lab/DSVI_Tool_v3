25ae300d9411f1c8533f371980c9c0963618b7e3
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-25 10:28:00 -0600

    changed baseURL in axios customFetch to render

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const customFetch = axios.create({
-  baseURL: 'http://localhost:3000/',
-  // baseURL: 'https://sdg-ai-lab-dsvi-tool-server.onrender.com/',
+  // baseURL: 'http://localhost:3000/',
+  baseURL: 'https://sdg-ai-lab-dsvi-tool-server.onrender.com/',
   // this option allows to receive cookies for this nextJS app
   withCredentials: true,
 })
